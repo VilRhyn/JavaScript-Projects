206c6c7aeaaf4616a9b22d9e19dda0dce90aece6
VilRhyn <65341113+VilRhyn@users.noreply.github.com>
2022-09-11 13:08:51 -0700

    Submission Assignment #1
    Updated index.html in Project_expressions_alert

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,21 @@
+window.alert("Hello, World!");
+
+document.write(' Hello, World!');
+
+
+var R = " Rhyn";
+document.write(R);
+window.alert(R);
+
+document.write(" Sally said, \"Let\'s go!\"")
+
+var ConStr = (" \"Change is inevitable; " + "growth is optional\"")
+document.write(ConStr)
+
+var family = "Jones", Dad = "Tim", Mom = "Kim", Kid = "Jim"
+
+function My_First_Function() { //Defining a function
+    var str = "This text is green!";//Defining a variable
+    var result = str.fontcolor("green");
+    document.getElementById("Green_Text").innerHTML = result;
+}
