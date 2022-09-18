1fe3ca8fc2497fd435513fda7a5281589ae16574
VilRhyn <65341113+VilRhyn@users.noreply.github.com>
2022-09-18 16:57:03 -0700

    Add files via upload

---
+++ Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,59 @@
+//Assignment #26 
+document.write(typeof "Word"); //displays 'string' in the browser 
+
+document.write(typeof 3); //displays 'number' in the browser
+
+//Assignment #27
+document.write(2E310);//infinity values
+
+document.write(-3E310);//negative infinity
+
+//Assignment #28
+
+document.write(10>2);//Boolean that displays 'true' in the browser
+
+document.write(10<2);//Boolean that displays 'false' in the browser
+//^Challenge #7^
+
+//Assignment #29
+
+console.log(2+2);//Displays '4' in the console of the page (f12)
+//^Challenge #7^
+
+//Assignment #30
+
+document.write("10" + 5);//Displays 105 in the browser (adding 5 onto the end of the string "10")
+//an example of coercion^
+
+//Assignment #31
+
+document.write(10==10);//==checks for equality 
+//^displays true in the browser
+
+document.write(3==11); //displays false in the browser
+
+//Assignment #32
+
+X = 10;
+Y = 10;
+document.write(X===Y);//displays true
+
+A = 82;
+B = "82";
+document.write(A===B);//displays false
+
+F = "Rhyn"
+U = "Rhyn"
+document.write(F === U);//displays true
+
+//Assignment #33
+
+document.write(5>2 && 10>4);//returns true
+
+document.write(5>10 && 10>4);//returns false
+
+document.write(5>10||10>4); //returns true 
+
+document.write(5>10||10>20);//returns false
+
+
